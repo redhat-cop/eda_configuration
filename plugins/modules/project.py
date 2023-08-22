42b495165b07653208b64aeb0b9f2079658bd147
Chris Renwick <crenwick@redhat.com>
2023-08-22 11:50:05 +0100

    added project role

--- plugins/modules/project.py
+++ plugins/modules/project.py
@@ -31,7 +31,7 @@ options:
       type: str
     new_name:
       description:
-        - Setting this option will change the existing name (looked up via the name field.
+        - Setting this option will change the existing name (looked up via the name field).
       type: str
     description:
       description:
