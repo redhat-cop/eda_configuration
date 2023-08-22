3ae02d8465fdc60e6461a0bf2919423e7edab41f
Tom Page <tompage1994@hotmail.co.uk>
2023-08-22 16:19:32 +0100

    Update plugins/doc_fragments/auth.py

--- plugins/doc_fragments/auth.py
+++ plugins/doc_fragments/auth.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) 2020, Sean Sullivan <@sean-m-sullivan>
+# Copyright: (c) 2023, Chris Renwick <@crenwick93>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
